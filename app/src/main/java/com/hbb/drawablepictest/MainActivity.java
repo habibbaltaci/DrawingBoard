fcf70a9a714442a56aeaa8f8aef5d226af6b694f
habibalt <hbaltaci@agdas.com.tr>
2022-12-20 14:45:41 +0300

    Initial commit

---
+++ app/src/main/java/com/hbb/drawablepictest/MainActivity.java
@@ -0,0 +1,43 @@
+package com.hbb.drawablepictest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawableView drawbleView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        drawbleView = (DrawableView) findViewById(R.id.drawable_view);
+
+        drawbleView.setDrawingEnabled(true);
+    }
+
+}
